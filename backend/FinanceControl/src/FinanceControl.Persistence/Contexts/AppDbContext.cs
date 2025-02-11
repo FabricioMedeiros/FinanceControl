cae9c84545756e1b5c76628ad85b406a67ee9be1
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-02-11 20:37:26 -0300

    Instalacao do pacote do Identity e criacao do DbContext na camada Persistence

---
+++ backend/FinanceControl/src/FinanceControl.Persistence/Contexts/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using FinanceControl.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceControl.Persistence.Contexts
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>(entity =>
+            {
+                entity.ToTable("Users");
+                entity.Property(e => e.FullName).IsRequired().HasMaxLength(60);
+            });
+        }
+    }
+}
