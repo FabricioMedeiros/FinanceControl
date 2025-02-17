368f4d86e8b2d1745d776c87f827c03a4b25d120
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-02-17 20:04:14 -0300

    Criacao do Notificator para trabalhar as respostas e noticiacoes da API

---
+++ backend/FinanceControl/src/FinanceControl.Application/Interfaces/INotificator.cs
@@ -0,0 +1,11 @@
+﻿using FinanceControl.Application.Notifications;
+
+namespace FinanceControl.Application.Interfaces
+{
+    public interface INotificator
+    {
+        List<Notification> GetNotifications();
+        void AddNotification(Notification notification);
+        bool HasNotification();
+    }
+}
---
+++ backend/FinanceControl/src/FinanceControl.Application/Notifications/Notificator.cs
@@ -0,0 +1,29 @@
+﻿using FinanceControl.Application.Interfaces;
+
+namespace FinanceControl.Application.Notifications
+{
+    public class Notificator : INotificator
+    {
+        private readonly List<Notification> _notifications;
+
+        public Notificator()
+        {
+            _notifications = new List<Notification>();
+        }
+
+        public List<Notification> GetNotifications()
+        {
+            return _notifications;
+        }
+
+        public void AddNotification(Notification notification)
+        {
+            _notifications.Add(notification);
+        }
+
+        public bool HasNotification()
+        {
+            return _notifications.Any();
+        }
+    }
+}
