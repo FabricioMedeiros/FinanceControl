fdd678dfd2efbcc6b7e05d38d97e9aadc819b52c
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-02-17 20:05:01 -0300

    Criacao do servico do JWT para geracao do token de autenticacao

---
+++ backend/FinanceControl/src/FinanceControl.Application/Services/JwtTokenService .cs
@@ -0,0 +1,65 @@
+﻿using FinanceControl.Application.Configurations;
+using FinanceControl.Application.Interfaces;
+using FinanceControl.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace FinanceControl.Application.Services
+{
+    public class JwtTokenService : IJwtTokenService
+    {
+        private readonly JwtSettings _jwtSettings;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public JwtTokenService(IOptions<JwtSettings> jwtSettings, UserManager<ApplicationUser> userManager)
+        {
+            _jwtSettings = jwtSettings.Value;
+            _userManager = userManager;
+        }
+
+        public async Task<string> GenerateTokenAsync(ApplicationUser user)
+        {
+            var claims = await GetClaims(user);
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.SecretKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _jwtSettings.Issuer,
+                audience: _jwtSettings.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(_jwtSettings.ExpiryInMinutes),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private async Task<IList<Claim>> GetClaims(ApplicationUser user)
+        {
+            var claims = new List<Claim>
+            {
+              new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+              new Claim("userId", user.Id),
+              new Claim("userName", user.FullName),
+              new Claim(JwtRegisteredClaimNames.Email, user.Email!),
+              new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+            var userClaims = await _userManager.GetClaimsAsync(user);
+            claims.AddRange(userClaims);
+
+            var roles = await _userManager.GetRolesAsync(user);
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            return claims;
+        }
+    }
+}
