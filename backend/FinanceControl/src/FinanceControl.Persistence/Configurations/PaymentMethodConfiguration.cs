b7d232e44c4cc66a5326e025b28d862421ee33cd
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-02-24 20:48:04 -0300

    Criacao das configuracao das entidades no Entity Framework

---
+++ backend/FinanceControl/src/FinanceControl.Persistence/Configurations/PaymentMethodConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using FinanceControl.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class PaymentMethodConfiguration : IEntityTypeConfiguration<PaymentMethod>
+{
+    public void Configure(EntityTypeBuilder<PaymentMethod> builder)
+    {
+        builder.HasKey(p => p.Id);
+        builder.Property(p => p.Name).IsRequired().HasMaxLength(50);
+        builder.Property(p => p.UserId).IsRequired();
+
+        builder.HasIndex(p => new { p.UserId, p.Name }).IsUnique();
+    }
+}
