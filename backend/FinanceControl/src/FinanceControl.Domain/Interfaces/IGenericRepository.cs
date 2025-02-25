4c17fc27a6c59f2bb813b6c2ef3374968d49258e
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-02-25 15:07:36 -0300

    Criacao da interface e do repositorio generico "GenericRepository"

---
+++ backend/FinanceControl/src/FinanceControl.Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace FinanceControl.Domain.Interfaces
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity?>> GetAllAsync(Expression<Func<TEntity, bool>>? filter = null, params Expression<Func<TEntity, object>>[] includeProperties);
+        Task<TEntity?> GetByIdAsync(Guid id);
+        Task<TEntity> AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(Guid id);
+        Task<int> SaveChangesAsync();
+    }
+}
