20e8489619032ba671e087499a172299be571e1d
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-03-12 15:20:21 -0300

    Criacao dos servicos

---
+++ backend/FinanceControl/src/FinanceControl.Application/Services/CategoryService.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using FinanceControl.Application.DTOs;
+using FinanceControl.Application.Interfaces;
+using FinanceControl.Domain.Entities;
+using FinanceControl.Domain.Interfaces;
+
+namespace FinanceControl.Application.Services
+{
+    public class CategoryService : GenericService<Category, CategoryDto>, ICategoryService
+    {
+        public CategoryService(
+            IGenericRepository<Category> repository,
+            IMapper mapper,
+            INotificator notificator)
+            : base(repository, mapper, notificator)
+        {
+        }
+    }
+}
---
+++ backend/FinanceControl/src/FinanceControl.Application/Services/TransactionService.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using FinanceControl.Application.DTOs;
+using FinanceControl.Application.Interfaces;
+using FinanceControl.Domain.Entities;
+using FinanceControl.Domain.Interfaces;
+
+namespace FinanceControl.Application.Services
+{
+    public class TransactionService : GenericService<Transaction, TransactionDto>, ITransactionService
+    {
+        public TransactionService(
+            IGenericRepository<Transaction> repository,
+            IMapper mapper,
+            INotificator notificator)
+            : base(repository, mapper, notificator)
+        {
+        }
+    }
+}
