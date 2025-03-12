c27492be2bdebc5568340e77ae9ebb93accb34e9
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-03-12 15:05:02 -0300

    Criacao dos repositorios

---
+++ backend/FinanceControl/src/FinanceControl.Persistence/Repositories/CategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using FinanceControl.Domain.Entities;
+using FinanceControl.Domain.Interfaces;
+using FinanceControl.Persistence.Contexts;
+using FinanceControl.Persistence.Repositories;
+
+namespace FinanceControl.Infrastructure.Repositories
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ backend/FinanceControl/src/FinanceControl.Persistence/Repositories/PaymentMethodRepository.cs
@@ -0,0 +1,14 @@
+﻿using FinanceControl.Domain.Entities;
+using FinanceControl.Domain.Interfaces;
+using FinanceControl.Persistence.Contexts;
+using FinanceControl.Persistence.Repositories;
+
+namespace FinanceControl.Infrastructure.Repositories
+{
+    public class PaymentMethodRepository : GenericRepository<PaymentMethod>, IPaymentMethodRepository
+    {
+        public PaymentMethodRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
