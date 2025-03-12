342fd049eb14162182dacce3ee0cf1ea604e270e
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-03-12 15:20:53 -0300

    Configuracao da injecao de dependencia dos servicos

--- backend/FinanceControl/src/FinanceControl.API/Configurations/DependencyInjectionConfiguration.cs
+++ backend/FinanceControl/src/FinanceControl.API/Configurations/DependencyInjectionConfiguration.cs
@@ -1,6 +1,9 @@
 ﻿using FinanceControl.Application.Interfaces;
 using FinanceControl.Application.Notifications;
 using FinanceControl.Application.Services;
+using FinanceControl.Domain.Interfaces;
+using FinanceControl.Infrastructure.Repositories;
+using FinanceControl.Persistence.Repositories;
 
 namespace FinanceControl.API.Configurations
 {
@@ -12,6 +15,16 @@ namespace FinanceControl.API.Configurations
             services.AddScoped<IJwtTokenService, JwtTokenService>();
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IPaymentMethodRepository, PaymentMethodRepository>();
+            services.AddScoped<ITransactionRepository, TransactionRepository>();
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IPaymentMethodService, PaymentMethodService>();
+            services.AddScoped<ITransactionService, TransactionService>();
+            services.AddScoped(typeof(IGenericService<,>), typeof(GenericService<,>));
 
             return services;
         }
