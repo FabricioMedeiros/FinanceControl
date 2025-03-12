a38f25ac8dfc834c595c3c57d181f21cda092e27
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-03-12 15:06:07 -0300

    Criacao do BaseService que será herdado pelo GenericService

---
+++ backend/FinanceControl/src/FinanceControl.Application/Services/BaseService.cs
@@ -0,0 +1,25 @@
+﻿using FinanceControl.Application.Interfaces;
+using FinanceControl.Application.Notifications;
+
+namespace FinanceControl.Application.Services
+{
+    public abstract class BaseService
+    {
+        private readonly INotificator _notificator;
+
+        protected BaseService(INotificator notificator)
+        {
+            _notificator = notificator;
+        }
+
+        protected void Notify(string message)
+        {
+            _notificator.AddNotification(new Notification(message));
+        }
+
+        protected bool IsValidOperation()
+        {
+            return !_notificator.HasNotification();
+        }
+    }
+}
