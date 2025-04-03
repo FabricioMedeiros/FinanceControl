76f4e5991a34ce3cfacfed52f77eeddfcd32befb
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-04-03 16:20:29 -0300

    Implementacao do "FilterBinder" para permitir realizar filtros de objetos complexos

---
+++ backend/FinanceControl/src/FinanceControl.API/Filters/FilterBinder.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace FinanceControl.API.Filters
+{
+    public class FilterBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null) throw new ArgumentNullException(nameof(bindingContext));
+
+            var filters = bindingContext.HttpContext.Request.Query
+                .Where(q => !string.IsNullOrWhiteSpace(q.Value))
+                .ToDictionary(
+                    q => q.Key.Replace("[", ".").Replace("]", ""),
+                    q => q.Value.ToString()
+                );
+
+            bindingContext.Result = ModelBindingResult.Success(filters);
+            return Task.CompletedTask;
+        }
+    }
+}
