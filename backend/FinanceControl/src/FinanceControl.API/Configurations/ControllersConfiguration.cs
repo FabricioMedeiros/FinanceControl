1c49410e4a110d5273e8646535d69b06d1e21643
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-04-03 16:32:07 -0300

    Implementacao das configuracoes das Controllers

---
+++ backend/FinanceControl/src/FinanceControl.API/Configurations/ControllersConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using FinanceControl.API.Filters;
+
+namespace FinanceControl.API.Configurations
+{
+    public static class ControllersConfiguration
+    {
+        public static void AddControllersConfiguration(this IServiceCollection services)
+        {
+            services.AddControllers(options => options.Filters.Add<ValidationFilter>())
+                    .AddJsonOptions(options =>
+                    {
+                        options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+                    });
+        }
+    }
+}
