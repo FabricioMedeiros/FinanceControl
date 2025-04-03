320cb50c97940f59716653b667209f03839b5a57
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-04-03 16:21:46 -0300

    Criacao do atrbituto ValidationContextAttribute para poder indentificar qual validator sera utilizado nos metodos da controller

---
+++ backend/FinanceControl/src/FinanceControl.API/Extensions/ValidationContextAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace FinanceControl.API.Extensions
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class ValidationContextAttribute : Attribute
+    {
+        public Type ValidatorType { get; }
+
+        public ValidationContextAttribute(Type validatorType)
+        {
+            ValidatorType = validatorType ?? throw new ArgumentNullException(nameof(validatorType));
+        }
+    }
+}
