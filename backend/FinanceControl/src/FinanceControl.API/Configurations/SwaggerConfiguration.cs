1380ff70c2eaf0ef2499bd37af45d6666d86dd09
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-04-03 16:33:48 -0300

    Implementacao das configuracoes do Swagger

---
+++ backend/FinanceControl/src/FinanceControl.API/Configurations/SwaggerConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace FinanceControl.API.Configurations
+{
+    public static class SwaggerConfiguration
+    {
+        public static void AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(opt =>
+            {
+                opt.SwaggerDoc("v1", new OpenApiInfo { Title = "FinanceControl API", Version = "v1" });
+                opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Por favor informe o token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "bearer"
+                });
+
+                opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+            });
+        }
+    }
+}
