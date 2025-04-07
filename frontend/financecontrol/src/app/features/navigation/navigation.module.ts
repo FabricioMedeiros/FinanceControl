c81c2be45948b97ffa708c95d991da6fa856b117
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-04-07 20:30:13 -0300

    Criacao do modulo navigation e dos componentes basicos de navegacao da aplicacao

---
+++ frontend/financecontrol/src/app/features/navigation/navigation.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NavigationRoutingModule } from './navigation-routing.module';
+import { NavigationComponent } from './navigation.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { MenuLoginComponent } from './components/menu-login/menu-login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ServiceUnavailableComponent } from './components/service-unavailable/service-unavailable.component';
+
+
+@NgModule({
+  declarations: [
+    NavigationComponent,
+    HomeComponent,
+    FooterComponent,
+    SidebarComponent,
+    HeaderComponent,
+    LogoComponent,
+    MenuLoginComponent,
+    NotFoundComponent,
+    ServiceUnavailableComponent
+  ],
+  imports: [
+    CommonModule,
+    NavigationRoutingModule
+  ]
+})
+export class NavigationModule { }
