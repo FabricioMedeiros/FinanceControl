a48e90994a2e6022fcde10bb3178dcc214951029
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-04-07 20:30:41 -0300

    Registrando o modulo navigation no modulo principal da aplicacao

--- frontend/financecontrol/src/app/app-routing.module.ts
+++ frontend/financecontrol/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'navigation', loadChildren: () => import('./features/navigation/navigation.module').then(m => m.NavigationModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
