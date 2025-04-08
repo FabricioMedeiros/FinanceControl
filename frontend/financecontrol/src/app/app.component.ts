34a6ddf598da9449972c1487f3474bb72eb7e8bc
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-04-08 20:31:34 -0300

    Implementacao do AppComponent para exibir os componentes basicos da aplicacao (header, footer e sidebar)

--- frontend/financecontrol/src/app/app.component.ts
+++ frontend/financecontrol/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'financecontrol';
+  showOnlyComponentRoute: boolean = false;
+  token: string | null = "";
+
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.updateShowOnlyComponentRoute(event.urlAfterRedirects);
+    });
+  }
+
+  private updateShowOnlyComponentRoute(url: string): void {
+    const specialRoutes = ['/account/login', '/account/register'];
+    this.showOnlyComponentRoute = specialRoutes.some(route => url.includes(route));
+  }
 }
