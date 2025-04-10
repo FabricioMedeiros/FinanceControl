1f9251ed4de7580bf81fd113d86471547db153d1
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-04-10 14:34:52 -0300

    Adicinando as rotas de login e registro de usuarios no modulo AccountModule

--- frontend/financecontrol/src/app/features/account/account-routing.module.ts
+++ frontend/financecontrol/src/app/features/account/account-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountComponent } from './account.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [{ path: '', component: AccountComponent }];
+const routes: Routes = [
+  {
+    path: '', component: AccountComponent,
+    children: [
+      { path: 'register', component: RegisterComponent },
+      { path: 'login', component: LoginComponent }
+    ]
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
