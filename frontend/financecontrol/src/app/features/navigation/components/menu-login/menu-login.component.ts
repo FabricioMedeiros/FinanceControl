2681cb2e0ac5ee3850868be45e0ba756fe371eaf
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-04-10 20:54:34 -0300

    Implementacao do componente MenuLogin para exibir o usuario logado e opcao de sair no header da aplicacao

--- frontend/financecontrol/src/app/features/navigation/components/menu-login/menu-login.component.ts
+++ frontend/financecontrol/src/app/features/navigation/components/menu-login/menu-login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { LocalStorageUtils } from 'src/app/core/utils/localstorage';
 
 @Component({
   selector: 'app-menu-login',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-login.component.css']
 })
 export class MenuLoginComponent {
+  token: string | null = "";
+  user: any;
+  email: string | null = "";
+  localStorageUtils = new LocalStorageUtils();
 
+  constructor(private router: Router) {  }
+
+  isUserLoggedIn(): boolean {
+    this.token = this.localStorageUtils.getTokenUser() ;
+    this.user = this.localStorageUtils.getUser();
+    this.email = this.localStorageUtils.getEmailUser();
+
+    return this.token !== null;
+  }
+
+  logout() {
+    this.localStorageUtils.clearLocalUserData();
+    this.router.navigate(['/account/login']);
+  }
 }
