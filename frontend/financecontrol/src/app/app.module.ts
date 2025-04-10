081306a770e9301401f0e9bffc5e80b6190cffde
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-04-10 11:50:00 -0300

    Configuracao do toaster, httpclient e localstorageultis no modulo principal da aplicacao para que os demais modulos possam utiliza-los

--- frontend/financecontrol/src/app/app.module.ts
+++ frontend/financecontrol/src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationModule } from './features/navigation/navigation.module';
+import { AccountModule } from './features/account/account.module';
+import { LocalStorageUtils } from './core/utils/localstorage';
+
 
 @NgModule({
   declarations: [
@@ -12,9 +19,14 @@ import { NavigationModule } from './features/navigation/navigation.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({ toastClass: 'ngx-toastr custom-toast', preventDuplicates: true }),
+    AccountModule,
     NavigationModule
   ],
-  providers: [],
+  providers: [LocalStorageUtils],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
