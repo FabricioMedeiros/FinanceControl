2b403af835ab34c0c3eee4693df6a3d26d586fef
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-04-10 19:59:53 -0300

    Registrando os componentes de login e registro no modulo AccountModule, configuracao das dependencias necessarias para trabalhar com formularios reativos, e configuracao do AccountService

--- frontend/financecontrol/src/app/features/account/account.module.ts
+++ frontend/financecontrol/src/app/features/account/account.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AccountRoutingModule } from './account-routing.module';
 import { AccountComponent } from './account.component';
-
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AccountService } from './services/account.service';
 
 @NgModule({
   declarations: [
-    AccountComponent
+    AccountComponent,
+    RegisterComponent, 
+    LoginComponent
   ],
   imports: [
     CommonModule,
-    AccountRoutingModule
+    AccountRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    AccountService  
   ]
 })
 export class AccountModule { }
