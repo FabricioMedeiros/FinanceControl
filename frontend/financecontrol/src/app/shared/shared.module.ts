df01d762a6fcda67fb29d6550778e49f579b785c
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-04-14 20:11:20 -0300

    Exclusao dos arquivos desnecessarios pois os components BaseFormComponent e BaseListComponent seram abstratos

--- frontend/financecontrol/src/app/shared/shared.module.ts
+++ frontend/financecontrol/src/app/shared/shared.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { BaseListComponent } from './components/base-list/base-list.component';
-import { BaseFormComponent } from './components/base-form/base-form.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { PaginationComponent } from './components/pagination/pagination.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -10,8 +8,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    BaseListComponent,
-    BaseFormComponent,
     SearchBarComponent,
     PaginationComponent
   ],
