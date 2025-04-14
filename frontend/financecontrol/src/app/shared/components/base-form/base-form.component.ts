a33c5853515e15f0f5c380d0bda56dfd0c1953f8
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-04-14 10:07:34 -0300

    Criacao dos componentes basicos de listagem, cadastro, pesquisa e paginacao que serao utilizados em toda a aplicacao

---
+++ frontend/financecontrol/src/app/shared/components/base-form/base-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-form',
+  templateUrl: './base-form.component.html',
+  styleUrls: ['./base-form.component.css']
+})
+export class BaseFormComponent {
+
+}
--- frontend/financecontrol/src/app/shared/shared.module.ts
+++ frontend/financecontrol/src/app/shared/shared.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
 import { SharedComponent } from './shared.component';
+import { BaseListComponent } from './components/base-list/base-list.component';
+import { BaseFormComponent } from './components/base-form/base-form.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
 
 
 @NgModule({
   declarations: [
-    SharedComponent
+    SharedComponent,
+    BaseListComponent,
+    BaseFormComponent,
+    SearchBarComponent,
+    PaginationComponent
   ],
   imports: [
     CommonModule,
