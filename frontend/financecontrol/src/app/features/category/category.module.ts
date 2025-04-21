a29426c6f376eb41c6838f61d206e3208969458a
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-04-21 18:26:51 -0300

    Adicionados os modulos ReactiveFormsModule, SharedModule, NgxSpinnerModule ao modulo CategoryModule para implementacao dos componentes de listagem e cadastro

--- frontend/financecontrol/src/app/features/category/category.module.ts
+++ frontend/financecontrol/src/app/features/category/category.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 import { CategoryRoutingModule } from './category-routing.module';
 import { CategoryComponent } from './category.component';
 import { CategoryListComponent } from './components/category-list/category-list.component';
 import { CategoryFormComponent } from './components/category-form/category-form.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
@@ -15,7 +18,10 @@ import { CategoryFormComponent } from './components/category-form/category-form.
   ],
   imports: [
     CommonModule,
-    CategoryRoutingModule
+    CategoryRoutingModule,
+    ReactiveFormsModule,
+    SharedModule,
+    NgxSpinnerModule
   ]
 })
 export class CategoryModule { }
