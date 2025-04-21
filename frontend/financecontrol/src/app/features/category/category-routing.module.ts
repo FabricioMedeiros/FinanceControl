14e89d1ba746e9882c4cdaaed452718fb3b661da
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-04-21 18:27:29 -0300

    Criacao das rotas do modulo CategoryModule

--- frontend/financecontrol/src/app/features/category/category-routing.module.ts
+++ frontend/financecontrol/src/app/features/category/category-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CategoryComponent } from './category.component';
 
-const routes: Routes = [{ path: '', component: CategoryComponent }];
+import { CategoryComponent } from './category.component';
+import { CategoryFormComponent } from './components/category-form/category-form.component';
+import { CategoryListComponent } from './components/category-list/category-list.component';
+import { authGuard } from 'src/app/core/guards/auth.guard';
+import { canDeactivateForm } from 'src/app/core/guards/can-deactivate-form.guard';
+import { genericResolver } from 'src/app/core/resolvers/generic-resolver';
+import { CategoryService } from 'src/app/shared/services/category.service';
+
+const routes: Routes = [{
+  path: '', component: CategoryComponent,
+  children: [
+    { path: '', redirectTo: 'list', pathMatch: 'full' },
+    { path: 'list', component: CategoryListComponent,  canActivate: [authGuard]  }, 
+    { path: 'new', component: CategoryFormComponent,  canActivate: [authGuard], canDeactivate: [canDeactivateForm] }, 
+    { path: 'edit/:id', component: CategoryFormComponent, 
+      canActivate: [authGuard],  
+      canDeactivate: [canDeactivateForm], resolve: {
+      category: genericResolver(CategoryService, (service, id) => service.getSpecialtyById(id))
+    }},
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
