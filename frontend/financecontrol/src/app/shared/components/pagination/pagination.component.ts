8c5c3dc4d4ca73ab53765091ffb5da8f902e7890
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-04-21 18:23:38 -0300

    Implementacao do componente de paginacao PaginationComponent

--- frontend/financecontrol/src/app/shared/components/pagination/pagination.component.ts
+++ frontend/financecontrol/src/app/shared/components/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./pagination.component.css']
 })
 export class PaginationComponent {
+  @Input() currentPage: number = 1;
+  @Input() totalPages: number = 1;
+  @Output() pageChanged = new EventEmitter<number>();
 
+  constructor() { }
+
+  get totalPagesArray(): number[] {
+    return Array.from({ length: this.totalPages }, (_, i) => i + 1);
+  }
+  
+  onNextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.pageChanged.emit(this.currentPage);
+    }
+  }
+  
+  onPreviousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.pageChanged.emit(this.currentPage);
+    }
+  }  
 }
