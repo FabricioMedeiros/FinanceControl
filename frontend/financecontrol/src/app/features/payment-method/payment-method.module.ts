69d36dfba304a0190f2f1fb1cf522daa229e8134
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-04-23 09:45:36 -0300

    Configuracao dos modulos ReactiveFormsModule, SharedModule, NgxSpinnerModule no PaymentMethodModule

--- frontend/financecontrol/src/app/features/payment-method/payment-method.module.ts
+++ frontend/financecontrol/src/app/features/payment-method/payment-method.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 import { PaymentMethodRoutingModule } from './payment-method-routing.module';
 import { PaymentMethodComponent } from './payment-method.component';
 import { PaymentMethodListComponent } from './components/payment-method-list/payment-method-list.component';
 import { PaymentMethodFormComponent } from './components/payment-method-form/payment-method-form.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
@@ -15,7 +19,10 @@ import { PaymentMethodFormComponent } from './components/payment-method-form/pay
   ],
   imports: [
     CommonModule,
-    PaymentMethodRoutingModule
+    PaymentMethodRoutingModule,
+    ReactiveFormsModule,
+    SharedModule,
+    NgxSpinnerModule
   ]
 })
 export class PaymentMethodModule { }
