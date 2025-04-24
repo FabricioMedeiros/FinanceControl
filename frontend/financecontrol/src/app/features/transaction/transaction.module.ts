641136d81c61db1b49d45cee1613b7b561d01b77
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-04-24 20:33:45 -0300

    Registrando os componentes do modulo de transacoes

--- frontend/financecontrol/src/app/features/transaction/transaction.module.ts
+++ frontend/financecontrol/src/app/features/transaction/transaction.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { TransactionRoutingModule } from './transaction-routing.module';
 import { TransactionComponent } from './transaction.component';
+import { TransactionListComponent } from './components/transaction-list/transaction-list.component';
+import { TransactionFormComponent } from './components/transaction-form/transaction-form.component';
 
 
 @NgModule({
   declarations: [
-    TransactionComponent
+    TransactionComponent,
+    TransactionListComponent,
+    TransactionFormComponent
   ],
   imports: [
     CommonModule,
