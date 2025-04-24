6f612df47e99a992a26a0acfaa4b1bdd2344eddf
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-04-24 20:18:23 -0300

    Implementacao do componente de listagem de formas de pagamento

--- frontend/financecontrol/src/app/features/payment-method/components/payment-method-list/payment-method-list.component.ts
+++ frontend/financecontrol/src/app/features/payment-method/components/payment-method-list/payment-method-list.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+
+import { BaseListComponent } from 'src/app/shared/components/base-list/base-list.component';
+import { PaymentMethod } from '../../models/payment-method';
+import { PaymentMethodService } from 'src/app/shared/services/payment-method.service';
 
 @Component({
   selector: 'app-payment-method-list',
   templateUrl: './payment-method-list.component.html',
   styleUrls: ['./payment-method-list.component.css']
 })
-export class PaymentMethodListComponent {
+export class PaymentMethodListComponent  extends BaseListComponent<PaymentMethod> implements OnInit {
+  constructor(paymentMethodService: PaymentMethodService,
+    router: Router,
+    toastr: ToastrService,
+    spinner: NgxSpinnerService,
+    modalService: BsModalService) {
+    super(paymentMethodService, router, toastr, spinner, modalService);
+  }
 
+  override ngOnInit(): void {
+    this.fieldSearch = 'name';
+    this.placeholderSearch = 'Pesquise pelo nome';
+    super.ngOnInit();
+  }
+
+  isCurrentRoute(url: string): boolean {
+    return url.includes('/payment-method');
+  }
 }
