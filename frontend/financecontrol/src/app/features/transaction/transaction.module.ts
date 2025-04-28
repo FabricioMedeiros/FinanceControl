04d7ea2bbc176fe3c23bc8bbc77d39d2fc725e7d
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-04-28 11:18:46 -0300

    Configuracao dos modulos ReactiveFormsModule, SharedModule, NgxSpinnerModule no TransactionModule

--- frontend/financecontrol/src/app/features/transaction/transaction.module.ts
+++ frontend/financecontrol/src/app/features/transaction/transaction.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 import { TransactionRoutingModule } from './transaction-routing.module';
 import { TransactionComponent } from './transaction.component';
 import { TransactionListComponent } from './components/transaction-list/transaction-list.component';
 import { TransactionFormComponent } from './components/transaction-form/transaction-form.component';
-
 
 @NgModule({
   declarations: [
@@ -15,7 +18,10 @@ import { TransactionFormComponent } from './components/transaction-form/transact
   ],
   imports: [
     CommonModule,
-    TransactionRoutingModule
+    TransactionRoutingModule,
+    ReactiveFormsModule,
+    SharedModule,
+    NgxSpinnerModule
   ]
 })
 export class TransactionModule { }
