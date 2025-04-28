2879d7ee96ae09624b02e5b53598943d20a43719
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-04-28 11:19:48 -0300

    Configuracao das rotas do modulo TransactionModule e correcao da rota de listagem de lancamentos no componente SideBar

--- frontend/financecontrol/src/app/features/transaction/transaction-routing.module.ts
+++ frontend/financecontrol/src/app/features/transaction/transaction-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TransactionComponent } from './transaction.component';
+import { authGuard } from 'src/app/core/guards/auth.guard';
+import { canDeactivateForm } from 'src/app/core/guards/can-deactivate-form.guard';
+import { genericResolver } from 'src/app/core/resolvers/generic-resolver';
+import { TransactionService } from 'src/app/shared/services/transaction.service';
+import { TransactionFormComponent } from './components/transaction-form/transaction-form.component';
+import { TransactionListComponent } from './components/transaction-list/transaction-list.component';
 
-const routes: Routes = [{ path: '', component: TransactionComponent }];
+const routes: Routes = [{
+  path: '', component: TransactionComponent,
+  children: [
+    { path: '', redirectTo: 'list', pathMatch: 'full' },
+    { path: 'list', component: TransactionListComponent,  canActivate: [authGuard]  }, 
+    { path: 'new', component: TransactionFormComponent,  canActivate: [authGuard], canDeactivate: [canDeactivateForm] }, 
+    { path: 'edit/:id', component: TransactionFormComponent, 
+      canActivate: [authGuard],  
+      canDeactivate: [canDeactivateForm], resolve: {
+      Transaction: genericResolver(TransactionService, (service, id) => service.getTransactionById(id))
+    }},
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
