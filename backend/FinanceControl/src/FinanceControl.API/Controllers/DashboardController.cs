dc53d3866e09765d7a3e6af78044603767e81daa
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-06-09 20:09:50 -0300

    Criacao da controller da Dashboard

---
+++ backend/FinanceControl/src/FinanceControl.API/Controllers/DashboardController.cs
@@ -0,0 +1,26 @@
+﻿using FinanceControl.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceControl.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    public class DashboardController : MainController
+    {
+        private readonly IDashboardService _dashboardService;
+
+        public DashboardController(IDashboardService dashboardService, INotificator notificator)
+            : base(notificator)
+        {
+            _dashboardService = dashboardService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetDashboard([FromQuery] int year, [FromQuery] int? month = null)
+        {
+            var result = await _dashboardService.GetDashboardDataAsync(year, month);
+            return CustomResponse(result);
+        }
+    }
+}
