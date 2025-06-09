2bc0de1495aef31843bbe1751889281246fd0a28
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-06-09 20:18:36 -0300

    Implementacao do metodo getTransactionsByPeriod para buscar as transacoes por periodo, refatoracao dos metodos saveLocalSearchTerm, getLocalSearchTerm e clearLocalSearchTerm para trabalhar com os filtros que serao utilizado no componente TransactionListComponent

--- frontend/financecontrol/src/app/shared/services/transaction.service.ts
+++ frontend/financecontrol/src/app/shared/services/transaction.service.ts
@@ -13,30 +13,59 @@ export class TransactionService extends BaseService {
 
     getAll(page?: number, pageSize?: number, field?: string, value?: string): Observable<any> {
         const headers = this.GetAuthHeaderJson();
-        
+
         let url = `${this.UrlServiceV1}transaction`;
-    
+
         if (page !== undefined && pageSize !== undefined) {
             url += `?pageNumber=${page}&pageSize=${pageSize}`;
         }
-    
+
         if (field && value) {
             url += `${(page !== undefined && pageSize !== undefined) ? '&' : '?'}filters[${field}]=${value}`;
         }
-    
+
         return this.http
             .get<any>(url, headers)
             .pipe(catchError(super.serviceError));
-    }     
+    }
+
+    getTransactionsByPeriod(
+        startDate: string,
+        endDate: string,
+        categoryId?: string,
+        paymentMethodId?: string,
+        page?: number,
+        pageSize?: number
+    ): Observable<any> {
+        const headers = this.GetAuthHeaderJson();
+
+        let url = `${this.UrlServiceV1}transaction/by-period?startDate=${startDate}&endDate=${endDate}`;
+
+        if (categoryId) {
+            url += `&categoryId=${categoryId}`;
+        }
+
+        if (paymentMethodId) {
+            url += `&paymentMethodId=${paymentMethodId}`;
+        }
+
+        if (page !== undefined && pageSize !== undefined) {
+            url += `&pageNumber=${page}&pageSize=${pageSize}`;
+        }
+
+        return this.http.get<any>(url, headers).pipe(
+            catchError(super.serviceError)
+        );
+    }
 
     getTransactionById(id: string): Observable<Transaction> {
         const headers = this.GetAuthHeaderJson();
 
         return this.http
-            .get<Transaction>(`${this.UrlServiceV1}transaction/${id}`,  headers)
+            .get<Transaction>(`${this.UrlServiceV1}transaction/${id}`, headers)
             .pipe(catchError(super.serviceError));
     }
-   
+
     registerTransaction(transaction: Transaction): Observable<Transaction> {
         const headers = this.GetAuthHeaderJson();
 
@@ -53,7 +82,7 @@ export class TransactionService extends BaseService {
         const httpOptions = {
             headers: headers
         };
-    
+
         return this.http
             .put<Transaction>(`${this.UrlServiceV1}transaction/${transaction.id}`, transaction, headers)
             .pipe(
@@ -61,7 +90,7 @@ export class TransactionService extends BaseService {
                 catchError(this.serviceError)
             );
     }
-    
+
     delete(id: string): Observable<any> {
         const headers = this.GetAuthHeaderJson();
 
@@ -82,15 +111,35 @@ export class TransactionService extends BaseService {
         localStorage.removeItem('currentPageTransactionList');
     }
 
-    saveLocalSearchTerm(searchTerm: string): void {
-        localStorage.setItem('searchTermTransactionList', searchTerm);
+    saveLocalSearchTerm(pageSize: number, category: string, paymentMethod: string, startDate: Date, endDate: Date): void {
+        if (pageSize) localStorage.setItem('pageSizeTransactionList', pageSize.toString());
+        if (startDate) localStorage.setItem('startDateFilterTransactionList', startDate.toString());
+        if (endDate) localStorage.setItem('endDateFilterTransactionList', endDate.toString());
+        if (category) localStorage.setItem('categoryFilterTransactionList', category);
+        if (paymentMethod) localStorage.setItem('paymentMethodFilterTransactionList', paymentMethod);
     }
 
-    getLocalSearchTerm(): string {
-        return localStorage.getItem('searchTermTransactionList') || '';
+    getLocalSearchTerm(): { pageSize?: number, category?: string, paymentMethod?: string, startDate?: Date, endDate?: Date } {
+        const pageSizeValue = localStorage.getItem('pageSizeTransactionList');
+        const categoryValue = localStorage.getItem('categoryFilterTransactionList');
+        const paymentMethodValue = localStorage.getItem('paymentMethodFilterTransactionList');
+        const startDateValue = localStorage.getItem('startDateFilterTransactionList');
+        const endDateValue = localStorage.getItem('endDateFilterTransactionList');
+
+        return {
+            pageSize: pageSizeValue ? parseInt(pageSizeValue, 10) : undefined,
+            category: categoryValue ? categoryValue : undefined,
+            paymentMethod: paymentMethodValue ? paymentMethodValue : undefined,
+            startDate: startDateValue ? new Date(startDateValue) : undefined,
+            endDate: endDateValue ? new Date(endDateValue) : undefined
+        };
     }
 
     clearLocalSearchTerm(): void {
-        localStorage.removeItem('searchTermTransactionList');
+        localStorage.removeItem('pageSizeTransactionList');
+        localStorage.removeItem('startDateFilterTransactionList');
+        localStorage.removeItem('endDateFilterTransactionList');
+        localStorage.removeItem('categoryFilterTransactionList');
+        localStorage.removeItem('paymentMethodFilterTransactionList');
     }
 }
