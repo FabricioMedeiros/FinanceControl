4cdeb12870c8f23f2a3261b790b82eeced0b7c4d
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-06-09 20:08:21 -0300

    Criacao da interface e do repositorio da Dashboard

---
+++ backend/FinanceControl/src/FinanceControl.Domain/Interfaces/IDashboardRepository.cs
@@ -0,0 +1,9 @@
+﻿using FinanceControl.Domain.Entities;
+
+namespace FinanceControl.Domain.Interfaces
+{
+    public interface IDashboardRepository
+    {
+        Task<IEnumerable<Transaction>> GetTransactionsAsync(int year, int? month = null);
+    }
+}
---
+++ backend/FinanceControl/src/FinanceControl.Infrastructure/Repositories/DashboardRepository.cs
@@ -0,0 +1,32 @@
+﻿using FinanceControl.Domain.Entities;
+using FinanceControl.Domain.Interfaces;
+using FinanceControl.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceControl.Infrastructure.Repositories
+{
+    public class DashboardRepository : IDashboardRepository
+    {
+        protected readonly AppDbContext _context;
+        public DashboardRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Transaction>> GetTransactionsAsync(int year, int? month = null)
+        {
+            var query = _context.Transactions
+                .Include(t => t.Category)
+                .Include(t => t.PaymentMethod)
+                .AsQueryable();
+
+            query = query.Where(t => t.Date.Year == year);
+
+            if (month.HasValue)
+                query = query.Where(t => t.Date.Month == month.Value);
+
+            return await query.ToListAsync();
+        }
+
+    }
+}
