ed3265376c0f1a8ae60dc5b500107260401e7dca
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2025-06-23 20:29:55 -0300

    Criacao do DashboardService para consumir o endpoint da dashboard na API

---
+++ frontend/financecontrol/src/app/features/dashboard/services/dashboard.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { catchError, map, tap } from "rxjs/operators";
+
+import { BaseService } from "src/app/core/services/base.service";
+
+@Injectable()
+export class DashboardService extends BaseService {
+
+    constructor(private http: HttpClient) { super(); }
+
+    getDashboardData(year: number, month?: number): Observable<any> {
+        const headers = this.GetAuthHeaderJson();
+
+        let url = `${this.UrlServiceV1}dashboard?year=${year}`;
+        if (month) {
+            url += `&month=${month}`;
+        }
+
+        return this.http
+            .get<any>(url, headers)
+            .pipe(catchError(super.serviceError));
+    }
+}
